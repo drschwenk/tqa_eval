aca28c5cea3f037fd1da42a522439d1749eabd6c
drschwenk <schwgit2@gmail.com>
2016-11-11 12:18:43 -0800

    added question subtype breakdowns

--- tqa_utils/evaluate.py
+++ tqa_utils/evaluate.py
@@ -21,8 +21,38 @@ class Evaluator(DataSetCommonTools):
         if not self.dataset:
             self.load_dataset()
         total_answered_counts, overall_expected_score = self.validate_answer_format(predicted_answers)
-        results = []
         questions_by_type = self.build_question_lookup(by_type=True)
+        questions_by_subtype = self.build_questions_by_subtype(questions_by_type['nonDiagramQuestions'])
+        # results = []
+        # expected_totals = {}
+        # expected_by_chance = {}
+        # for question_type, questions in questions_by_type.items():
+        #     expected_totals[question_type] = len(questions)
+        #     expected_by_chance[question_type] = self.expected_score_by_chance(questions.values())
+        #     for q_id, answer in predicted_answers.items():
+        #         if q_id in questions.keys():
+        #             if self.answered_correctly(questions, q_id, answer):
+        #                 results.append(question_type)
+        # correct_by_type = Counter(results)
+        # correct_by_type['overall'] = sum(correct_by_type.values())
+        # accuracies = self.compute_accuracies(correct_by_type, questions_by_type)
+        # total_answered_counts['overall'] = sum(total_answered_counts.values())
+        # expected_totals['overall'] = sum(expected_totals.values())
+        # expected_by_chance['overall'] = overall_expected_score
+        # results_to_tabulate = {
+        #     'accuracy': accuracies,
+        #     'total correct': correct_by_type,
+        #     'number of questions expected': expected_totals,
+        #     'number of questions answered': total_answered_counts,
+        #     'baseline accuracy (random guesses)': expected_by_chance
+        # }
+        self.print_results(self.tabulate_results_by_type(questions_by_type, predicted_answers, overall_expected_score, total_answered_counts))
+        print('Non-Diagram Question Type Breakdown')
+        self.print_results(self.tabulate_results_by_type(questions_by_subtype, predicted_answers, overall_expected_score))
+        return
+
+    def tabulate_results_by_type(self, questions_by_type, predicted_answers, overall_expected_score, total_answered_counts=0,):
+        results = []
         expected_totals = {}
         expected_by_chance = {}
         for question_type, questions in questions_by_type.items():
@@ -35,18 +65,18 @@ class Evaluator(DataSetCommonTools):
         correct_by_type = Counter(results)
         correct_by_type['overall'] = sum(correct_by_type.values())
         accuracies = self.compute_accuracies(correct_by_type, questions_by_type)
-        total_answered_counts['overall'] = sum(total_answered_counts.values())
         expected_totals['overall'] = sum(expected_totals.values())
         expected_by_chance['overall'] = overall_expected_score
         results_to_tabulate = {
             'accuracy': accuracies,
             'total correct': correct_by_type,
             'number of questions expected': expected_totals,
-            'number of questions answered': total_answered_counts,
             'baseline accuracy (random guesses)': expected_by_chance
         }
-        self.print_results(results_to_tabulate)
-        return accuracies
+        if total_answered_counts:
+            total_answered_counts['overall'] = sum(total_answered_counts.values())
+            results_to_tabulate['number of questions answered'] = total_answered_counts
+        return results_to_tabulate
 
     def compute_accuracies(self, correct_by_type, questions_by_type):
         accuracy_by_type = {}
@@ -93,12 +123,20 @@ class Evaluator(DataSetCommonTools):
             print('errors found ')
             for qid, error in errors.items():
                 print(qid, ' ', error)
-
         answered_questions_total = {
             'diagramQuestions': len([qid for qid in predicted_answers if qid[0] == 'D']),
             'nonDiagramQuestions': len([qid for qid in predicted_answers if qid[0] == 'N'])
         }
         return answered_questions_total, overall_expected_score
+
+    def build_questions_by_subtype(self, nd_questions):
+        subtypes = ["True or False", "Multiple Choice", "Matching"]
+        questions_by_subtype = defaultdict(dict)
+        for sub_type in subtypes:
+            questions_by_subtype[sub_type] = {qid: question for qid, question in nd_questions.items()
+                                              if question['questionSubType'] == sub_type}
+        return questions_by_subtype
+
 
     def print_results(self, results):
         build_results = defaultdict(list)
@@ -108,3 +146,4 @@ class Evaluator(DataSetCommonTools):
                 build_results[q_type].extend(["{0:.3f}".format(result)])
         display_results = [[k] + v for k, v in sorted(build_results.items())]
         print(tabulate(display_results, headers, tablefmt="fancy_grid"))
+
